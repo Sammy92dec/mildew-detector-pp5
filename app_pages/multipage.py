53a3eb02c798bd7b0a7eca8bc592b81266e4ed6b
Samrawit Tekheste <samrawit.samuel.tk@gmail.com>
2024-11-20 18:08:35 +0000

    Remove: Navaigation from the dashboard.Go back to juptyer_notebook to check the data

--- app_pages/multipage.py
+++ app_pages/multipage.py
@@ -2,25 +2,18 @@ import streamlit as st
 import matplotlib.pyplot as plt
 
 
+# Class to generate multiple Streamlit pages using an object oriented approach
 class MultiPage:
-    """Framework for managing multiple Streamlit pages."""
-    
+
     def __init__(self, app_name):
         self.pages = []
         self.app_name = app_name
 
+        
     def add_page(self, title, func):
-        """Add a new page to the app."""
         self.pages.append({"title": title, "function": func})
 
     def run(self):
-        # Render the sidebar with a dropdown to select the page
-        st.sidebar.title(self.app_name)
-        page = st.sidebar.selectbox(
-            "Navigate to:",
-            self.pages,
-            format_func=lambda page: page["title"]
-        )
-
-        # Run the selected page's function
-        page["function"]()
+        st.title(self.app_name)
+        page = st.sidebar.radio('Menu', self.pages, format_func=lambda page: page['title'])
+        page['function']()
